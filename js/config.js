5d93d413eb67758527fb1cf379a9988ba2353c08
jinxianggongyi <3780027024@qq.com>
2025-03-26 20:58:41 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const supabase = createClient(
+    'https://mbvtwdmpiedjbnxwtnet.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1idnR3ZG1waWVkamJueHd0bmV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4OTkyNjQsImV4cCI6MjA1ODQ3NTI2NH0.dtl0NgeMvmCU8WlfiH72KZYXdkeSYdYJ24QytbssQoA'
+);
+
+export const ADMIN_CODE = 'ANDMIN33685';
